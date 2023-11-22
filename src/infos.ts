6103511558d4fc4263ba02cd66db9e5bfd23b3e2
z.karahacane <z.karahacane@actongroup.com>
2023-11-22 18:36:44 +0100

    v0.0.3 => v0.0.4

--- src/infos.ts
+++ src/infos.ts
@@ -1,7 +1,7 @@
 // @ts-nocheck
 
 import type { ServiceInfos } from '@dso-console/server/src/plugins/services.js'
-import { apimUrl } from './utils'
+import { apimUrl } from './utils.js'
 
 
 const infos: ServiceInfos = {

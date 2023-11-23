e97744cf93c64600dcaaedc1610b8f69c5523ae5
ArnaudTa <33383276+ArnaudTA@users.noreply.github.com>
2023-11-23 10:24:57 +0100

    fix:

--- src/infos.ts
+++ src/infos.ts
@@ -8,7 +8,7 @@ const infos: ServiceInfos = {
   name: 'apim',
   to: () => `${apimUrl}`,
   title: 'Apim Gravitee',
-  imgSrc: '/img/gitlab.svg',
+  imgSrc: 'https://www.gravitee.io/hubfs/Gravitee_2022/Images/logo-dark.svg',
   description: 'Apim est un service d\'apim.',
 }
 
